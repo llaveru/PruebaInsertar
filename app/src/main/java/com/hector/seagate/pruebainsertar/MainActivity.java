373ed47fde8858b022bc1e5ca4764dcaea911d78
llaveru <el.llaveru@gmail.com>
2017-06-10 02:02:06 +0200

    se añaden comentarios

--- app/src/main/java/com/hector/seagate/pruebainsertar/MainActivity.java
+++ app/src/main/java/com/hector/seagate/pruebainsertar/MainActivity.java
@@ -440,6 +440,24 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                 e.printStackTrace();
             }
             BufferedWriter bw = null;
+
+            /*
+            EXPLICACION DE URL.Encoder
+
+            Utility class for HTML form encoding.
+            This class contains static methods for converting a String to the application/x-www-form-urlencoded MIME format.
+
+When encoding a String, the following rules apply:
+
+    The alphanumeric characters "a" through "z", "A" through "Z" and "0" through "9" remain the same.
+    The special characters ".", "-", "*", and "_" remain the same.
+    The space character "   " is converted into a plus sign "+".
+    All other characters are unsafe and are first converted into one or more bytes using some encoding scheme.
+    Then each byte is represented by the 3-character string "%xy", where xy is the two-digit hexadecimal representation of the byte.
+     The recommended encoding scheme to use is UTF-8.
+     However, for compatibility reasons, if an encoding is not specified, then the default encoding of the platform is used.
+
+             */
             try {
                 bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
 
